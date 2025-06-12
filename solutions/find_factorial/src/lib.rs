f08f7d8f53f0966d9b6277215e5f02dc61da2853
obenali <oussama.benali@ump.ac.ma>
2025-06-12 15:24:11 +0100

    first quest done

--- solutions/find_factorial/src/lib.rs
+++ solutions/find_factorial/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn factorial(num: u64) -> u64 {
     let mut result : u64 = 1 ; 
-    for n in 1..num {
+    for n in 1..=num {
         result = result*n 
     }
-    result * num 
+    result 
 }
