3ad5a39b4b52ebd391376534de77f1b8ad14383e
obenali <oussama.benali@ump.ac.ma>
2025-06-13 21:51:59 +0100

    Rust also supports structs that look similar to tuples, called tuple structs. Tuple structs have the added meaning the struct name provides but don’t have names associated with their fields; rather, they just have the types of the fields. Tuple structs are useful when you want to give the whole tuple a name and make the tuple a different type from other tuples, and when naming each field as in a regular struct would be verbose or redundant

---
+++ solutions/name_initials/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut ni : Vec<String> = Vec::new() ; 
+
+    for name in names {
+        // 
+        let first_last = n.split(' ');
+        let mut one : String::New() ; 
+        for s in first_last {
+                one.push(s.prefix()) 
+            if index < fist_last.len() {
+                one.push(". ")
+            }
+            
+        }
+        ni.push(n.to_string());
+    }
+    ni
+}
