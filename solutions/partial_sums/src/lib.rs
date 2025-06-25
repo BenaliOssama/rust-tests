a629bea98199630d560ae28d4a7fb88f9b65a8f5
obenali <oussama.benali@ump.ac.ma>
2025-06-26 00:39:25 +0100

    use the copied method

--- solutions/partial_sums/src/lib.rs
+++ solutions/partial_sums/src/lib.rs
@@ -1,11 +1,7 @@
 pub fn parts_sums(arr: &[u64]) -> Vec<u64>{
     let mut res : Vec<u64> = [].to_vec();
     for i in (0..arr.len()).rev()  {
-        let mut sum = 0 ; 
-        for j in 0..=i {
-            sum += arr[j];
-        }
-        res.push(sum);
+        res.push(arr[0..=i].iter().copied().reduce(|a, b| a + b).unwrap());
     }
     res.push(0);
     res
