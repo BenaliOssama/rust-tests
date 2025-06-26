0ef772ffd8e7c65219b77482b2f90525890d2dd7
obenali <oussama.benali@ump.ac.ma>
2025-06-26 06:54:03 +0100

    alot of hacks

--- solutions/counting_words/src/lib.rs
+++ solutions/counting_words/src/lib.rs
@@ -7,13 +7,10 @@ pub fn counting_words(words: &str) -> HashMap<String, u32> {
         return s.chars().filter(|c| (*c).is_alphanumeric() || (*c) == '\'' ).collect::<String>().to_lowercase();
     }).collect();
     for mut word in words {
-        // if word != ""{
-        //     if word.contains('\''){
-        //         let ss = word.split('\'').collect::<Vec<&str>>();//.collect::<Vec<&str>>().join("\'");
-        //         word = format!("{}\\'{}", ss[0], ss[1]);//ss[0].to_owned() + "\\'" + &ss[1];
-        //     }
+        if word != ""{
+            word = word.trim_matches('\'').to_string();
             let count = map.entry(word).or_insert(0);
-            // *count += 1 ;
+            *count += 1 ;
         }
     }
     map
